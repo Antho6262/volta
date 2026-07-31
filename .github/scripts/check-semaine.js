60d5917982b507729ad15786ed70325fd338c194
Antho6262 <a.malheiro@hotmail.fr>
2026-07-31 23:04:59 +0200

    fix: forcer la fermeture du process (firebase-admin gardait la connexion ouverte, run infini)

--- .github/scripts/check-semaine.js
+++ .github/scripts/check-semaine.js
@@ -165,7 +165,9 @@ async function main() {
   }
 }
 
-main().catch((e) => {
-  console.error("Erreur:", e);
-  process.exit(1);
-});
+main()
+  .then(() => process.exit(0))
+  .catch((e) => {
+    console.error("Erreur:", e);
+    process.exit(1);
+  });
